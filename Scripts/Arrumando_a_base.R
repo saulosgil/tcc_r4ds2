a6a1c1ecd3a69cdbd7f80ae2054d0bd4d0c204f3
saulosgil <saulosgil@hotmail.com>
2021-04-27 11:18:47 -0300

    Faxinando a base para as analises

--- Scripts/Arrumando_a_base.R
+++ Scripts/Arrumando_a_base.R
@@ -1,10 +1,14 @@
 
+"%>%" <- magrittr::"%>%"
+
 # Lendo a base de dados
 
 base_dados_bruta <-
   readr::read_csv("data-raw/NCD_PAA.csv",
                   col_names = TRUE,
-                  skip = 1) %>%
+                  # col_types = "cdcnnnnn",
+                  skip = 1
+                  ) %>%
   dplyr::glimpse()
 
 # Arrumando a base de dados
@@ -24,11 +28,29 @@ base_de_dados <-
     "Pais" = Country,
     "Ano" = Year,
     "Grupo_de_idade" = `Age Group`,
-    "Inatividade_física_em_ambos_os_sexos (% [ajustado pela idade])" = `Both sexes`,
-    "Inatividade_física_no_sexo_masculino (% [ajustado pela idade])" = Male,
-    "Inatividade_física_no_sexo_feminino (% [ajustado pela idade])" = Female
+    "Ina_ambos_sexos" = `Both sexes`,
+    "Ina_masc" = Male,
+    "Ina_fem" = Female
   ) %>%
-  dplyr::glimpse()
+  dplyr::mutate(
+    # Arrumando as linhas (stringr)
+    Ina_ambos_sexos = stringr::str_remove_all(Ina_ambos_sexos, "\\[.*\\]"),
+    Ina_ambos_sexos = stringr::str_replace_all(Ina_ambos_sexos, "No data", "NA"),
+    Ina_masc = stringr::str_remove_all(Ina_masc, "\\[.*\\]"),
+    Ina_masc = stringr::str_replace_all(Ina_masc, "No data", "NA"),
+    Ina_fem = stringr::str_remove_all(Ina_fem, "\\[.*\\]"),
+    Ina_fem = stringr::str_replace_all(Ina_fem, "No data", "NA")
+  ) %>%
+  dplyr::mutate(
+    # Atribuindo as caracteristicas de cada vetor
+    "Pais" = as.character(Pais),
+    "Ano" = as.double(Ano),
+    "Grupo de idade" = as.character("Grupo de idade"),
+    "Ina_ambos_sexos" = as.double(Ina_ambos_sexos),
+    "Ina_masc" = as.double(Ina_masc),
+    "Ina_fem" = as.double(Ina_fem),
+  ) %>%
+  dplyr::glimpse() %>%
+  tibble::view()
 
-View(base_de_dados)
-
+readr::write_rds(x = base_de_dados, file = "data/base_de_dados.rds")
